d5017de54a0c238218f0b5973a8b121b16c86cb7
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-18 20:21:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 require 'pry'
+=======
+# code here!
+>>>>>>> 05c217266b753bebc5f9a257305a5a8822e9e738
 class School
   def initialize (name)
     @name = name
@@ -9,7 +13,11 @@ class School
     if @roster.include?(grade)
       @roster[grade] << name
     else
+<<<<<<< HEAD
       @roster[grade] = []
+=======
+      @roster[grade] = ()
+>>>>>>> 05c217266b753bebc5f9a257305a5a8822e9e738
       @roster[grade] <<  name
     end
   end
@@ -17,10 +25,14 @@ class School
     @grade = @roster[grade]
   end
   def sort
+<<<<<<< HEAD
     new_roster = {}
     @roster.each do |a, b|
       new_roster[a] = b.sort
     end
       new_roster
+=======
+    @roster.sort
+>>>>>>> 05c217266b753bebc5f9a257305a5a8822e9e738
   end
 end
